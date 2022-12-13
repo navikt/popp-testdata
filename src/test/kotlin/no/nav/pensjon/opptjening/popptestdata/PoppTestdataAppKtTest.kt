2dcf208c8076a328a5f1ca169f555e3ca71830bd
Mo Amini <mo.amini@nav.no>
2022-12-13 13:23:06 +0100

    Added authentication on /inntekt
    Co-authored-by: eivind.askestad@nav.no

---
+++ src/test/kotlin/no/nav/pensjon/opptjening/popptestdata/PoppTestdataAppKtTest.kt
@@ -0,0 +1,94 @@
+package no.nav.pensjon.opptjening.popptestdata
+
+import com.nimbusds.jose.JOSEObjectType
+import no.nav.pensjon.opptjening.popptestdata.api.Environment
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest(classes = [PgiEndringApplication::class])
+@AutoConfigureMockMvc
+@DirtiesContext
+@EnableMockOAuth2Server
+internal class PoppTestdataAppKtTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var server: MockOAuth2Server
+
+
+    @Test
+    fun `Given a valid lagreInntekt request when calling post inntekt then return 200 ok`() {
+        mockMvc.perform(
+            post("/inntekt")
+                .contentType(APPLICATION_JSON)
+                .content(createLagreInntektRequest())
+                .header(HttpHeaders.AUTHORIZATION, token(acceptedAudience))
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+
+    @Test
+    fun `Given an unauthrorized audience when calling post inntekt then return 401`() {
+        mockMvc.perform(
+            post("/inntekt")
+                .contentType(APPLICATION_JSON)
+                .content(createLagreInntektRequest())
+                .header(HttpHeaders.AUTHORIZATION, token("banan"))
+        )
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    }
+
+    private fun createLagreInntektRequest(
+        environment: String? = Environment.Q1.name,
+        fnr: String? = "01234567890",
+        fomAar: Int? = 2000,
+        tomAar: Int? = 2001,
+        belop: Long? = 100000L,
+        redusertMedGrunnbelop: Boolean? = false
+    ): String {
+        return """
+            {
+                "environment": "$environment",
+                "fnr": "$fnr",
+                "fomAar": $fomAar,
+                "tomAar": $tomAar,
+                "belop": $belop,
+                "redusertMedGrunnbelop": $redusertMedGrunnbelop
+            }
+        """
+    }
+
+    private fun token(audience: String): String {
+        return "Bearer ${
+            server.issueToken(
+                issuerId = "aad",
+                clientId = "theclientid",
+                tokenCallback = DefaultOAuth2TokenCallback(
+                    "aad",
+                    "random",
+                    JOSEObjectType.JWT.type,
+                    listOf(audience),
+                    emptyMap(),
+                    3600
+                )
+            ).serialize()
+        }"
+    }
+
+    companion object {
+        private const val acceptedAudience = "testaud"
+    }
+}
