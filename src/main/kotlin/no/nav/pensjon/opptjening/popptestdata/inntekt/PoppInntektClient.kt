84096330c41f9aade7c38744bc2c06e18bb3b278
eivind <eivind.askestad@nav.no>
2022-12-19 22:14:20 +0100

    PoppUrlEnvironmentRouting to PoppUrlRouting
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/pensjon/opptjening/popptestdata/inntekt/PoppInntektClient.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/popptestdata/inntekt/PoppInntektClient.kt
@@ -1,14 +1,14 @@
 package no.nav.pensjon.opptjening.popptestdata.inntekt
 
 import no.nav.pensjon.opptjening.popptestdata.environment.Environment
-import no.nav.pensjon.opptjening.popptestdata.environment.PoppUrlEnvironmentRouting
+import no.nav.pensjon.opptjening.popptestdata.environment.PoppUrlRouting
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 
 @Component
 class PoppInntektClient(
-    private val urlRouting: PoppUrlEnvironmentRouting,
+    private val urlRouting: PoppUrlRouting,
     private val restTemplate: RestTemplate
 ) {
 
--- src/test/kotlin/no/nav/pensjon/opptjening/popptestdata/common/environment/PoppUrlRoutingTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/popptestdata/common/environment/PoppUrlRoutingTest.kt
@@ -1,26 +1,26 @@
 package no.nav.pensjon.opptjening.popptestdata.common.environment
 
 import no.nav.pensjon.opptjening.popptestdata.environment.Environment
-import no.nav.pensjon.opptjening.popptestdata.environment.PoppUrlEnvironmentRouting
+import no.nav.pensjon.opptjening.popptestdata.environment.PoppUrlRouting
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class PoppUrlEnvironmentRoutingTest {
+internal class PoppUrlRoutingTest {
 
     companion object {
         private const val poppUrlQ1 = "UrlQ1"
         private const val poppUrlQ2 = "UrlQ2"
     }
 
-    private val poppUrlEnvironmentRouting = PoppUrlEnvironmentRouting(poppUrlQ1, poppUrlQ2)
+    private val poppUrlRouting = PoppUrlRouting(poppUrlQ1, poppUrlQ2)
 
     @Test
     fun `Given Q1 as input When get url Then return Q1 url`() {
-        assertEquals(poppUrlQ1, poppUrlEnvironmentRouting.getUrl(Environment.Q1))
+        assertEquals(poppUrlQ1, poppUrlRouting.getUrl(Environment.Q1))
     }
 
     @Test
     fun `Given Q2 as input When get url Then return Q1 url`() {
-        assertEquals(poppUrlQ2, poppUrlEnvironmentRouting.getUrl(Environment.Q2))
+        assertEquals(poppUrlQ2, poppUrlRouting.getUrl(Environment.Q2))
     }
 }
