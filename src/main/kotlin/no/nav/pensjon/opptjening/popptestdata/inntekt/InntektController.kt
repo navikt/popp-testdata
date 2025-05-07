6b1a9455e1463d69997238782d669796bd6efc93
Arild Birkeland <arild.birkeland@nav.no>
2025-05-07 14:34:04 +0200

    chore(): verify that app is not in use

--- src/main/kotlin/no/nav/pensjon/opptjening/popptestdata/inntekt/InntektController.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/popptestdata/inntekt/InntektController.kt
@@ -9,10 +9,18 @@ import no.nav.pensjon.opptjening.popptestdata.inntekt.model.LagreInntektRequest
 import no.nav.pensjon.opptjening.popptestdata.miljo.Miljo
 import no.nav.pensjon.opptjening.popptestdata.token.TokenInterceptor.Companion.MILJO
 import no.nav.security.token.support.core.api.Protected
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
+
 
 @RestController
 @RequestMapping("/api/v1/inntekt")
@@ -21,6 +29,7 @@ class InntektController(private val inntektService: InntektService) {
 
     companion object {
         const val FIRST_FOM_YEAR = 1968
+        val log = LoggerFactory.getLogger(InntektController::class.java)
     }
 
     @PostMapping
@@ -33,6 +42,9 @@ class InntektController(private val inntektService: InntektService) {
         requestRequirement(request.fomAar <= request.tomAar) { "FomAr is grater than tomAr in request. fomAar was ${request.fomAar} and tomAar was ${request.tomAar}" }
         requestRequirement(request.fomAar >= FIRST_FOM_YEAR) { "FomAr is before $FIRST_FOM_YEAR. fomAar was ${request.fomAar}" }
         requestRequirement(request.tomAar <= LocalDate.now().year) { "TomAr is after current year ${LocalDate.now().year}. fomAar was ${request.tomAar}" }
+
+        println("Lagrer inntekter for fomAar=${request.fomAar} tomAar=${request.tomAar}")
+        log.info("Lagrer inntekter for fomAar=${request.fomAar} tomAar=${request.tomAar}")
 
         inntektService.lagreInntekter(request, miljo)
         return ResponseEntity.ok(HttpStatus.OK)
